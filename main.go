9fbe57ae8132e911fc75aa72b19cf365004a9383
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-12-12 14:46:21 +0000

    Feat: Debug Flag and Access Handler

--- main.go
+++ main.go
@@ -1,9 +1,45 @@
-package mappier
+package main
 
 import (
-	"fmt"
+	"log"
+	"flag"
+	"github.com/gofiber/fiber/v2"
 )
 
+var IsDebug bool
+
+func init() {
+	flag.BoolVar(&IsDebug, "D", false, "ログを出力する")
+}
+
+func debugAccessHandler(c *fiber.Ctx) error {
+	return c.Status(200).JSON(
+		fiber.Map{
+			"success": true,
+			"version": "0.0.1",
+		},
+	)
+}
+
+func defaultAccessHandler(c *fiber.Ctx) error {
+	return c.Status(200).JSON(
+		fiber.Map{
+			"success": true,
+		},
+	)
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	flag.Parse()
+
+	app := fiber.New()
+
+	if IsDebug {
+		app.Get("/", debugAccessHandler)
+		log.Fatal(app.Listen(":3000"))
+	} else {
+		app.Get("/", defaultAccessHandler)
+		app.Listen(":3000")
+	}
+
 }
